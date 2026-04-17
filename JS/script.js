8e4c59b41fc4e08753212c9902febe8c3c3c9557
FelipeJose067 <felipelipee1234@gmail.com>
2026-04-17 14:55:47 -0400

    TASK 02
    alteração no portifolio e finalização

--- JS/script.js
+++ JS/script.js
@@ -48,3 +48,12 @@ function enviarWhats(event) {
 
     window.open(url, '_blank');
 }
+
+$(document).ready(function(){
+    $('.mobile-btn').on('click', function(){
+        $('.mobile-menu').toggleClass('active');
+        $(this).find('i').toggleClass('fa-bars fa-x');
+    
+    })
+})
+
